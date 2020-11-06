70cc0973c750e499065c12ef1507638f84c13ee1
Nana Lau <nanalau315@hotmail.com>
2020-11-06 23:34:45 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,11 +7,7 @@ def key_for_min_value(name_hash)
   name_hash.each do |key, value|
     if value < min_num
       min_num = value
-      
-      
-
     end
-      
   end
   p name_hash.key(min_num)
 end
